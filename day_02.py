212fa9a23a73390d18119026e8ffad497a8ef4d7
Eyal Lin <eyal.lin@gmail.com>
2018-12-03 15:31:06 +0200

    day2 - part 2

--- day_02.py
+++ day_02.py
@@ -39,7 +39,30 @@ def test_analyze_entry():
     assert analyze_entry("ababab") == (0, 1)
 
 
+def day2b(data):
+    strings = data.split()
+    for i, s1 in enumerate(strings):
+        for s2 in strings[i+1:]:
+            res = ''.join(a for a, b in zip(s1, s2) if a == b)
+            if len(res) == len(s1) - 1:
+                return res
+
+
+def test_day2b():
+    input = """
+abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz
+"""
+    assert day2b(input) == 'fgij'
+
+
 if __name__ == "__main__":
     with open('day2_input.txt') as f:
         data = f.read()
     print(day2a(data))
+    print(day2b(data))
