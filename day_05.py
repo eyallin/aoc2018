3bd5cf66cf758a6d9f6a115f706018c7bee7b405
Eyal Lin <eyal.lin@gmail.com>
2018-12-09 09:53:22 +0200

    day5 - part 2, and performance improvement

--- day_05.py
+++ day_05.py
@@ -1,27 +1,47 @@
 # https://adventofcode.com/2018/day/5
 
 
-
 def react(s):
+    units = list(set(c.lower() for c in s))
+    pairs = ['{}{}'.format(c, c.upper()) for c in units] + ['{}{}'.format(c.upper(), c) for c in units]
+    
+    orig = s
     while True:
-        print(len(s))
-        for i, c2 in enumerate(s[1:], 1):
-            c1 = s[i-1]
-            if c1.lower() == c2.lower() and c1 != c2:
-                if i == 1:
-                    s = s[2:]
-                else:
-                    s = s[:i-1] + s[i+1:]
-                break
-        else:
+        for pair in pairs:
+            s = s.replace(pair, '')
+        if orig == s:
             return s
+        orig = s
 
 
 def test_react():
     s = 'dabAcCaCBAcCcaDA'
     assert len(react(s)) == 10
 
+
+def improve_reaction(s):
+    s = react(s)
+    units = set(c.lower() for c in s)
+    unit_results = {}
+    for unit in units:
+        s2 = s.replace(unit, '').replace(unit.upper(), '')
+        s2 = react(s2)
+        unit_results[unit] = s2
+    return min(unit_results.items(), key=lambda x: len(x[1]))
+
+
+def test_improvement():
+    s = 'dabAcCaCBAcCcaDA'
+    c, s = improve_reaction(s)
+    assert c == 'c'
+    assert s == 'daDA'
+
+
 if __name__ == "__main__":
     with open('day5_input.txt') as f:
         data = f.read()
-    print(react(data))
+
+    print(len(react(data)))
+
+    c, seq = improve_reaction(data)
+    print(len(seq))
